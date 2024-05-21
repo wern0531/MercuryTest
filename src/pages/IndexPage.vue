24739e5a414f4c193cfe33407947a2776bb0ed89
wern0531 <wern780531@hotmail.com>
2024-05-21 14:03:39 +0800

    create

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,182 @@
+<template>
+  <q-page class="row q-pt-xl">
+    <div class="full-width q-px-xl">
+      <div class="q-mb-xl">
+        <q-input v-model="tempData.name" label="姓名" />
+        <q-input v-model="tempData.age" label="年齡" />
+        <q-btn color="primary" class="q-mt-md" @click="addData">新增</q-btn>
+      </div>
+
+      <q-table
+        flat
+        bordered
+        ref="tableRef"
+        :rows="blockData"
+        :columns="(tableConfig as QTableProps['columns'])"
+        row-key="id"
+        hide-pagination
+        separator="cell"
+        :rows-per-page-options="[0]"
+      >
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <q-th v-for="col in props.cols" :key="col.name" :props="props">
+              {{ col.label }}
+            </q-th>
+            <q-th></q-th>
+          </q-tr>
+        </template>
+
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              style="min-width: 120px"
+            >
+              <div>{{ col.value }}</div>
+            </q-td>
+            <q-td class="text-right" auto-width v-if="tableButtons.length > 0">
+              <q-btn
+                @click="handleClickOption(btn, props.row)"
+                v-for="(btn, index) in tableButtons"
+                :key="index"
+                size="sm"
+                color="grey-6"
+                round
+                dense
+                :icon="btn.icon"
+                class="q-ml-md"
+                padding="5px 5px"
+              >
+                <q-tooltip
+                  transition-show="scale"
+                  transition-hide="scale"
+                  anchor="top middle"
+                  self="bottom middle"
+                  :offset="[10, 10]"
+                >
+                  {{ btn.label }}
+                </q-tooltip>
+              </q-btn>
+            </q-td>
+          </q-tr>
+        </template>
+        <template v-slot:no-data="{ icon }">
+          <div
+            class="full-width row flex-center items-center text-primary q-gutter-sm"
+            style="font-size: 18px"
+          >
+            <q-icon size="2em" :name="icon" />
+            <span> 無相關資料 </span>
+          </div>
+        </template>
+      </q-table>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { QTableProps } from 'quasar';
+import { ref, onMounted } from 'vue';
+interface btnType {
+  label: string;
+  icon: string;
+  status: string;
+}
+const blockData = ref([
+  // {
+  //   name: '',
+  //   age: '',
+  //   id: '',
+  // },
+]);
+const tableConfig = ref([
+  {
+    label: '姓名',
+    name: 'name',
+    field: 'name',
+    align: 'left',
+  },
+  {
+    label: '年齡',
+    name: 'age',
+    field: 'age',
+    align: 'left',
+  },
+]);
+const tableButtons = ref([
+  {
+    label: '編輯',
+    icon: 'edit',
+    status: 'edit',
+  },
+  {
+    label: '刪除',
+    icon: 'delete',
+    status: 'delete',
+  },
+]);
+
+const tempData = ref({
+  name: '',
+  age: '',
+});
+
+// answer
+const getAllData = () => {
+  axios.get('https://dahua.metcfire.com.tw/api/CRUDTest/a').then((res) => {
+    res.data.map((item) => {
+      blockData.value.push(item);
+    });
+  });
+};
+
+const addData = () => {
+  axios
+    .post('https://dahua.metcfire.com.tw/api/CRUDTest', tempData.value)
+    .then((res) => {
+      getAllData();
+    });
+  tempData.value.name = '';
+  tempData.value.age = '';
+};
+
+const handleClickOption = (btn, data) => {
+  if (btn.status === 'delete') {
+    handleDelete(data);
+  } else if (btn.status === 'edit') {
+    handleEdit(data);
+  }
+};
+
+const handleDelete = (data) => {
+  console.log(data.id);
+  if (confirm(`確定要刪除 ${data.name} ?`)) {
+    axios.delete(`https://dahua.metcfire.com.tw/api/CRUDTest/${data.id}`);
+  }
+};
+
+// const handleEdit = (data) => {
+
+//   axios.patch()
+// };
+
+// answer
+onMounted(() => {
+  getAllData();
+});
+</script>
+
+<style lang="scss" scoped>
+.q-table th {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.q-table tbody td {
+  font-size: 18px;
+}
+</style>
